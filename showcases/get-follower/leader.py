b30a561f6b0ec8f0f3ba00c644936905455b4a1b
Francisco Meirelles Fleury <franmeifleury@gmail.com>
2025-04-27 18:38:11 +0200

    adding get-follower showcase

---
+++ showcases/get-follower/leader.py
@@ -0,0 +1,43 @@
+from gradysim.protocol.interface import IProtocol
+from gradysim.protocol.messages.telemetry import Telemetry
+from gradysim.protocol.messages.mobility import GotoCoordsMobilityCommand
+from gradysim.protocol.messages.communication import SendMessageCommand
+from gradysim.protocol.plugin.statistics import create_statistics, finish_statistics
+from message import GetPositionMessage, PositionMessage
+from test_info import TEST_NAME
+
+class LeaderProtocol(IProtocol):
+
+    def _go_to_next_point(self):
+        self.next_point_id = (self.next_point_id + 1) % 4
+        self.next_point = self.points[self.next_point_id]
+        self.provider.send_mobility_command(GotoCoordsMobilityCommand(self.next_point[0], self.next_point[1], self.next_point[2]))
+
+    def initialize(self) -> None:
+
+        create_statistics(self, TEST_NAME)
+
+        self.points = [[100,100,50], [100,-100,50], [-100,-100,50], [-100,100,50]]
+        self.initial_pos = self.points[self.provider.get_id()]
+        self.current_pos = self.initial_pos
+        self.next_point_id = self.provider.get_id()
+        self._go_to_next_point()
+        self.provider.schedule_timer("next_point", self.provider.current_time() + 20)
+
+    def handle_timer(self, timer: str) -> None:
+        if timer == "next_point":
+            self._go_to_next_point()
+            if self.next_point_id != self.provider.get_id():
+                self.provider.schedule_timer("next_point", self.provider.current_time() + 20)
+    def handle_telemetry(self, telemetry: Telemetry) -> None:
+        self.current_pos = (telemetry.current_position[0], telemetry.current_position[1], telemetry.current_position[2])
+        self.provider.tracked_variables["position"] = self.current_pos
+
+    def handle_packet(self, message: str) -> None:
+        message = GetPositionMessage.from_json(message)
+
+        pos_message = PositionMessage(self.current_pos[0], self.current_pos[1], self.current_pos[2])
+        self.provider.send_communication_command(SendMessageCommand(pos_message.to_json(), message.sender_id))
+
+    def finish(self) -> None:
+        finish_statistics(self)
---
+++ showcases/get-follower/main.py
@@ -0,0 +1,25 @@
+from gradysim.simulator.handler.communication import CommunicationHandler, CommunicationMedium
+from gradysim.simulator.handler.mobility import MobilityHandler
+from gradysim.simulator.handler.ardupilot_mobility import ArdupilotMobilityHandler
+from gradysim.simulator.handler.timer import TimerHandler
+from gradysim.simulator.handler.visualization import VisualizationHandler
+from gradysim.simulator.simulation import SimulationBuilder, SimulationConfiguration, PositionScheme
+from leader import LeaderProtocol
+from follower import FollowerProtocol
+
+
+def main():
+    builder = SimulationBuilder(SimulationConfiguration(duration=30, debug=True, real_time=True))
+    builder.add_handler(CommunicationHandler(CommunicationMedium(transmission_range=500)))
+    builder.add_handler(TimerHandler())
+    builder.add_handler(ArdupilotMobilityHandler())
+    builder.add_handler(VisualizationHandler())
+
+    builder.add_node(LeaderProtocol, (0, 0, 20))
+    builder.add_node(FollowerProtocol, (0, 0, 20))
+
+    simulation = builder.build()
+    simulation.start_simulation()
+
+if __name__ == "__main__":
+    main()
